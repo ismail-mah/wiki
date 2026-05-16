7b45d52a2576e96ebd48516c498ad2fb50ae4403
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2026-05-16 17:23:12 +0200

    chore: add environment configuration for local and production

--- wiki/settings.py
+++ wiki/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
